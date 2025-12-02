6d6c27c1400dcba14b69ffc09f4b371814195060
Solomon Rosenfeld <solomon.rosenfeld@gmail.com>
2025-12-02 01:39:28 -0600

    all science questions now contain images

--- questionBanks.js
+++ questionBanks.js
@@ -10,11 +10,11 @@ window.QUESTION_BANKS = {
       { question: "In which year did WWI begin?", answer: "1914" }
     ],
     science: [
-      { question: "What gas do plants release during photosynthesis?", answer: "Oxygen" },
+      { question: "What gas do plants release during photosynthesis?", answer: "Oxygen", image: "https://cdn.britannica.com/36/227036-138-76B8E698/animation-photosynthesis-anabolic-reaction.jpg" },
       { question: "Which planet is known as the Red Planet?", answer: "Mars", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/300px-OSIRIS_Mars_true_color.jpg" },
-      { question: "What is the chemical symbol for gold?", answer: "Au" },
-      { question: "Which organ pumps blood throughout the human body?", answer: "The heart" },
-      { question: "What is the process by which liquids turn into gas called?", answer: "Evaporation" }
+      { question: "What is the chemical symbol for gold?", answer: "Au", image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Gold_bullion_ap_001.JPG" },
+      { question: "Which organ pumps blood throughout the human body?", answer: "The heart", image: "https://media.istockphoto.com/id/177443269/photo/male-full-body-circulatory-system-highlights-heart.jpg?s=612x612&w=0&k=20&c=itYjNlzQRzdBaw1dRDWGrKyru61zOAhTwTuJf1iwdJ0=" },
+      { question: "What is the process by which liquids turn into gas called?", answer: "Evaporation", image: "https://cdn.britannica.com/79/163379-138-536A3C0C.jpg" }
     ],
     test: [
       { question: "JOST-Box test: What is 2 + 2?", answer: "4" },
