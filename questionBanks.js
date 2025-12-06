5945a37ed43df71aa23b8f6b70d00d3627f58c05
Solomon Rosenfeld <solomon.rosenfeld@gmail.com>
2025-12-05 19:57:03 -0600

    shortened game version

--- questionBanks.js
+++ questionBanks.js
@@ -16,6 +16,11 @@ window.QUESTION_BANKS = {
       { question: "Which organ pumps blood throughout the human body?", answer: "The heart", image: "https://media.istockphoto.com/id/177443269/photo/male-full-body-circulatory-system-highlights-heart.jpg?s=612x612&w=0&k=20&c=itYjNlzQRzdBaw1dRDWGrKyru61zOAhTwTuJf1iwdJ0=" },
       { question: "What is the process by which liquids turn into gas called?", answer: "Evaporation", image: "https://cdn.britannica.com/79/163379-138-536A3C0C.jpg" }
     ],
+    scienceSHORT: [
+      { question: "Which planet is known as the Red Planet?", answer: "Mars", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/300px-OSIRIS_Mars_true_color.jpg" },
+      { question: "What is the chemical symbol for gold?", answer: "Au", image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Gold_bullion_ap_001.JPG" },
+      { question: "Which organ pumps blood throughout the human body?", answer: "The heart", image: "https://media.istockphoto.com/id/177443269/photo/male-full-body-circulatory-system-highlights-heart.jpg?s=612x612&w=0&k=20&c=itYjNlzQRzdBaw1dRDWGrKyru61zOAhTwTuJf1iwdJ0=" },
+    ],
     test: [
       { question: "JOST-Box test: What is 2 + 2?", answer: "4" },
       { question: "Live test: Spell the project name backwards.", answer: "xoB-TSOJ" },
